25978ea44e6461f7c92fb1b04813f841da1ae042
oumou004 <HE202558@students.ephec.be>
2026-08-15 13:59:14 +0200

    Ajout de app.py

--- app.py
+++ app.py
@@ -6,3 +6,34 @@ from classes.subject import Subject
 from classes.status import Status
 from classes.question import Question
 from classes.answer import Answer
+from textual.app import App
+from screens.menu import MenuScreen
+from screens.login import LoginScreen
+from screens.quiz import QuizScreen
+from screens.setting import SettingScreen
+
+
+class QuizApp(App):
+    def __init__(self):
+        super().__init__()
+        self.session = Session()
+        self.game = Game()
+        self.quiz = Quiz()
+        self.user = User()
+        self.question = Question()
+        self.subject = Subject()
+        self.answer = Answer()
+        self.status = Status()
+
+    SCREENS = {
+        "menu": MenuScreen,
+        "login": LoginScreen,
+        "setting": SettingScreen,
+        "quiz" : QuizScreen
+    }
+
+    def on_mount(self):
+        self.push_screen(MenuScreen())
+
+    if __name__ == "__main__":
+        pass
