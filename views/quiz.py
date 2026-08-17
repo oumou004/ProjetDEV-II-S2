dbd1b5fff41272c41b84ff466bf7a1e1d1125cc5
oumou004 <HE202558@students.ephec.be>
2026-08-17 21:54:48 +0200

    Ajout des zones pour organiser la page

--- views/quiz.py
+++ views/quiz.py
@@ -5,3 +5,31 @@ class QuizPage(tk.Frame):
         super().__init__(controller)
 
         self.controller = controller
+
+        # Zone 1
+        self.top_frame = tk.Frame(self)
+        self.top_frame.pack(
+            fill="both",
+            expand=True
+        )
+
+        # Zone 2
+        self.center_frame = tk.Frame(self)
+        self.center_frame.pack(
+            fill="both",
+            expand=True
+        )
+
+        # Zone 3
+        self.form_frame = tk.Frame(self)
+        self.form_frame.pack(
+            fill="both",
+            expand=True
+        )
+
+        # Zone 4
+        self.bottom_frame = tk.Frame(self)
+        self.bottom_frame.pack(
+            fill="both",
+            expand=True
+        )
