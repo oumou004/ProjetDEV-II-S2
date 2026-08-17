cf1129ad7ad17a289bbe906f683e8100bfa13b79
oumou004 <HE202558@students.ephec.be>
2026-08-17 22:38:18 +0200

    Ajout des zones

--- views/settings.py
+++ views/settings.py
@@ -8,3 +8,31 @@ class SettingsPage(tk.Frame):
         super().__init__(controller)
 
         self.controller = controller
+
+        # Zone 1
+        self.top_frame = tk.Frame(self)
+        self.top_frame.pack(
+            fill="both",
+            expand=True
+        )
+
+        # Zone 2
+        self.center_frame = tk.Frame(self)
+        self.center_frame.pack(
+            fill="both",
+            expand=True
+        )
+
+        # Zone 3
+        self.form_frame = tk.Frame(self)
+        self.form_frame.pack(
+            fill="both",
+            expand=True
+        )
+
+        # Zone 4
+        self.bottom_frame = tk.Frame(self)
+        self.bottom_frame.pack(
+            fill="both",
+            expand=True
+        )
