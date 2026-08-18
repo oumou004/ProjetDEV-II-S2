0328d71ec1929e850277624752636d737a311484
oumou004 <HE202558@students.ephec.be>
2026-08-18 17:15:18 +0200

    Update menu.py

--- screens/menu.py
+++ screens/menu.py
@@ -1,5 +1,6 @@
 from textual.screen import Screen
-
+from textual.widgets import Label, Button
+from pyfiglet import Figlet
 
 class MenuScreen(Screen):
 
@@ -53,3 +54,57 @@ class MenuScreen(Screen):
         }
 
         """
+
+    def compose(self):
+
+        titre = Figlet(font="doom")
+
+        yield Label(
+            titre.renderText("Feu Vert"),
+            id="title"
+        )
+
+        txt = """
+        ╔══════════════════════════════════════════════════════╗
+        ║          FEU VERT TERMINAL v1.0                  ║
+        ║                                                  ║
+        ║  PROJET DE DEVELOPPEMENT INFORMATIQUE 2           ║
+        ║                                                  ║
+        ║  Système intelligent de révision                  ║
+        ║  du permis de conduire théorique                  ║
+        ║                                                  ║
+        ║  Développeurs : Jeremie Nsenda, Oumou Fofana     ║
+        ║  Niveau       : EXPERT MODE                       ║
+        ║  Statut       : ONLINE                            ║
+        ║                                                  ║
+        ║  Conçu par des grands développeurs                ║
+        ║  dans le monde de l'informatique                  ║
+        ║                                                  ║
+        ╚══════════════════════════════════════════════════════╝
+        """
+
+        yield Label(txt, id="welcome")
+
+        yield Button(r"\[ CONNEXION ]", id="login")
+        yield Button(r"\[ REVISION ]", id="quiz")
+        yield Button(r"\[ REGLAGES ]", id="setting")
+
+
+    def on_button_pressed(self, event):
+
+        if event.button.id == "login":
+            self.app.push_screen("login")
+
+        elif event.button.id == "quiz":
+            if not self.app.session.is_connected:
+                self.app.push_screen("login")
+                return
+
+            self.app.push_screen("quiz")
+
+        elif event.button.id == "setting":
+            if not self.app.session.is_connected:
+                self.app.push_screen("login")
+                return
+
+            self.app.push_screen("setting")
