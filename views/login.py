9f0c9de438f0a1195759eb0540d2184ccd6a80f0
The Ghostwriter 25 <nsendjeremie@gmail.com>
2026-08-18 10:31:24 +0200

    implémentation des boutons

--- views/login.py
+++ views/login.py
@@ -49,3 +49,72 @@ class LoginPage(tk.Frame):
             fg="white"
         )
         self.title.pack(pady=20)
+        
+        btn_menu = tk.Button(
+            self.center_frame,
+            text="[ MENU ]",
+            width = 25,
+            height = 2,
+            bg = "#00A86B",
+            fg = "white",
+            font = ("Arial", 11, "bold"),
+            command = lambda:
+            controller.show_page("MenuPage")
+        )
+        btn_menu.pack(pady=5)
+
+
+        self.info = tk.Label(
+            self.bottom_frame,
+            text="",
+            bg="#121212",
+            fg="#00FF99",
+            font=("Arial", 11)
+        )
+        self.info.pack(pady=10)
+
+
+        btn_login = tk.Button(
+            self.center_frame,
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
+            text="[ Se connecter ]",
+            command = self.login_form
+        )
+        btn_login.pack(pady=5)
+
+
+        btn_logout = tk.Button(
+            self.center_frame,
+            text="[ Se déconnecter ]",
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
+            command=self.logout
+        )
+        btn_logout.pack(pady=5)
+
+
+        btn_add_user = tk.Button(
+            self.center_frame,
+            text="[ Créer un compte ]",
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
+            command=self.create_user_form
+        )
+        btn_add_user.pack(pady=5)
+        
+        
+        
+        
+        
+        
+        
