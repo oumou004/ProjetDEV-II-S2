13f33229fe6c38df89c0ca44e5a13b0e2acf3553
oumou004 <HE202558@students.ephec.be>
2026-08-20 14:39:24 +0200

    Update setting.py

--- views/setting.py
+++ views/setting.py
@@ -2,85 +2,215 @@ import tkinter as tk
 from tkinter import ttk
 from classes.database_manager import DatabaseError
 from classes.user import AuthenticationError, UserNotFoundError
-import re
+
 
 class SettingsPage(tk.Frame):
     def __init__(self, controller):
-        super().__init__(controller)
+        super().__init__(controller, bg="#121212")
 
         self.controller = controller
 
         # Zone 1
-        self.top_frame = tk.Frame(self)
+        self.top_frame = tk.Frame(self, bg="#121212")
         self.top_frame.pack(
+            fill="x",
+            pady=20
+        )
+
+        # Zone 2
+        self.middle_frame = tk.Frame(self, bg="#121212")
+        self.middle_frame.pack(expand=True, fill="both")
+
+        # Zone 2 à droite
+        self.form_frame = tk.Frame(self.middle_frame, bg="#121212")
+        self.form_frame.pack(
+            side="right",
+            padx=20,
+            pady=20,
             fill="both",
             expand=True
         )
 
-        # Zone 2
-        self.center_frame = tk.Frame(self)
+        # Zone 2 à gauche
+        self.center_frame = tk.Frame(self.middle_frame, bg="#121212")
         self.center_frame.pack(
+            side="left",
+            padx=20,
+            pady=20,
             fill="both",
             expand=True
         )
 
         # Zone 3
-        self.form_frame = tk.Frame(self)
-        self.form_frame.pack(
-            fill="both",
-            expand=True
+        self.bottom_frame = tk.Frame(self, bg="#121212")
+        self.bottom_frame.pack(
+            side="bottom",
+            pady=20
         )
 
-        # Zone 4
-        self.bottom_frame = tk.Frame(self)
-        self.bottom_frame.pack(
-            fill="both",
-            expand=True
-        )
 
         self.title = tk.Label(
             self.top_frame,
-            text="Bienvenue sur la page de reglages",
-            font=("Arial", 20)
+            text="Bienvenue sur Feu Vert\nReglages",
+            font=("Arial", 22, "bold"),
+            bg="#121212",
+            fg="white"
         )
-        self.title.pack(pady=20)
+        self.title.grid(row=0, column=0, padx=5, pady=5, sticky="e")
+
 
         btn_menu = tk.Button(
             self.top_frame,
             text="[ MENU ]",
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
             command=lambda:
             controller.show_page("MenuPage")
         )
+        btn_menu.grid(row=0, column=1, columnspan=2, pady=15)
 
-        btn_menu.pack()
 
         self.info = tk.Label(
             self.bottom_frame,
-            text=""
+            text="",
+            bg="#121212",
+            fg="white",
+            font=("Arial", 11)
         )
-
         self.info.pack(pady=10)
+
 
         btn_update_name = tk.Button(
             self.center_frame,
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
             text="[ Modifier le nom d'utilisateur ]",
-            command=self.update_username_form
+            command = self.update_username_form
         )
         btn_update_name.pack()
 
+
         btn_update_passwd = tk.Button(
             self.center_frame,
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
             text="[ Modifier le mot de passe ]",
-            command=self.update_passwd_form
+            command = self.update_passwd_form
         )
         btn_update_passwd.pack()
 
-        btn_ = tk.Button(
+
+        btn_remove_user = tk.Button(
             self.center_frame,
-            text="[ ]",
-            command=""
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
+            text="[ Supprimer un compte ]",
+            command = self.delete_user_form
         )
-        btn_.pack()
+        btn_remove_user.pack()
+
+
+        btn_add_subject = tk.Button(
+            self.center_frame,
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
+            text="[ Ajouter un sujet ]",
+            command = self.add_subject_form
+        )
+        btn_add_subject.pack()
+
+
+        btn_update_subject = tk.Button(
+            self.center_frame,
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
+            text="[ Modifier un sujet ]",
+            command = self.update_subject_form
+        )
+        btn_update_subject.pack()
+
+
+        btn_delete_subject = tk.Button(
+            self.center_frame,
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
+            text="[ Supprimer un sujet ]",
+            command = self.delete_subject_form
+        )
+        btn_delete_subject.pack()
+
+
+        btn_add_question = tk.Button(
+            self.center_frame,
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
+            text="[ Ajouter une question ]",
+            command = self.add_question_form
+        )
+        btn_add_question.pack()
+
+
+        btn_upddate_question = tk.Button(
+            self.center_frame,
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
+            text="[ Modifier une question ]",
+            command = self.update_question_form
+        )
+        btn_upddate_question.pack()
+
+
+        btn_delete_question = tk.Button(
+            self.center_frame,
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
+            text="[ Supprimer une question ]",
+            command = self.delete_question_form
+        )
+        btn_delete_question.pack()
+
+        btn_add_question = tk.Button(
+            self.center_frame,
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
+            text="[ Ajouter une reponse ]",
+            command=self.add_answer_form
+        )
+        btn_add_question.pack()
+
 
     def show_message(self, message):
         self.info.config(
@@ -91,37 +221,75 @@ class SettingsPage(tk.Frame):
         for widget in self.form_frame.winfo_children():
             widget.destroy()
 
-
     def update_username_form(self):
         self.clear_form()
         self.show_message("")
 
-        tk.Label(self.form_frame, text="Nom d'utilisateur").pack()
-        self.username = tk.Entry(self.form_frame)
-        self.username.pack()
+        tk.Label(
+            self.form_frame,
+            text="Nom d'utilisateur",
+            bg="#121212",
+            fg="white",
+            font=("Arial", 11)
+            ).grid(row=0, column=0, padx=5, pady=5, sticky="e")
+        self.username = tk.Entry(
+            self.form_frame,
+            width=30,
+            bg="#2B2B2B",
+            fg="white",
+            insertbackground="white"
+        )
+        self.username.grid(row=0, column=1, padx=5, pady=5, sticky="e")
 
-        tk.Label(self.form_frame, text="Nouveau nom d'utilisateur").pack()
-        self.new_username = tk.Entry(self.form_frame)
-        self.new_username.pack()
+        tk.Label(
+            self.form_frame,
+            text="Nouveau nom d'utilisateur",
+            bg="#121212",
+            fg="white",
+            font=("Arial", 11)
+        ).grid(row=1, column=0, padx=5, pady=5, sticky="e")
+        self.new_username = tk.Entry(
+            self.form_frame,
+            width=30,
+            bg="#2B2B2B",
+            fg="white",
+            insertbackground="white"
+        )
+        self.new_username.grid(row=1, column=1, padx=5, pady=5, sticky="e")
 
-        tk.Label(self.form_frame, text="Mot de passe").pack()
-        self.password = tk.Entry(self.form_frame, show="*")
-        self.password.pack()
+        tk.Label(
+            self.form_frame,
+            text="Mot de passe",
+            bg="#121212",
+            fg="white",
+            font=("Arial", 11)
+        ).grid(row=2, column=0, padx=5, pady=5, sticky="e")
+        self.password = tk.Entry(
+            self.form_frame,
+            width=30,
+            bg="#2B2B2B",
+            fg="white",
+            insertbackground="white",
+            show="*"
+        )
+        self.password.grid(row=2, column=1, padx=5, pady=5, sticky="e")
 
         tk.Button(
             self.form_frame,
-            text="Valider",
+            text="[ Valider ]",
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
             command=self.update_username
-        ).pack()
+        ).grid(row=3, column=0, columnspan=2, pady=15)
+
 
     def update_username(self):
         username = self.username.get().strip()
         new_username = self.new_username.get().strip()
         passwd = self.password.get().strip()
-
-        if not re.match("^[a-zA-Z0-9]+$", username) or not re.match("^[a-zA-Z0-9]+$", new_username):
-            self.show_message("Le nom d'utilisateur contient des caractères non autorisés")
-            return
 
         try:
 
@@ -136,37 +304,78 @@ class SettingsPage(tk.Frame):
             print(str(e))
             return
 
-     
+
+
+
     def update_passwd_form(self):
         self.clear_form()
         self.show_message("")
 
-        tk.Label(self.form_frame, text="Nom d'utilisateur").pack()
-        self.username = tk.Entry(self.form_frame)
-        self.username.pack()
+        tk.Label(
+            self.form_frame,
+            text="Nom d'utilisateur",
+            bg="#121212",
+            fg="white",
+            font=("Arial", 11)
+        ).grid(row=0, column=0, padx=5, pady=5, sticky="e")
+        self.username = tk.Entry(
+            self.form_frame,
+            width=30,
+            bg="#2B2B2B",
+            fg="white",
+            insertbackground="white"
+        )
+        self.username.grid(row=0, column=1, padx=5, pady=5, sticky="e")
 
-        tk.Label(self.form_frame, text="Mot de passe").pack()
-        self.password = tk.Entry(self.form_frame, show="*")
-        self.password.pack()
+        tk.Label(
+            self.form_frame,
+            text="Mot de passe",
+            bg="#121212",
+            fg="white",
+            font=("Arial", 11)
+        ).grid(row=1, column=0, padx=5, pady=5, sticky="e")
+        self.password = tk.Entry(
+            self.form_frame,
+            width=30,
+            bg="#2B2B2B",
+            fg="white",
+            insertbackground="white",
+            show="*"
+        )
+        self.password.grid(row=1, column=1, padx=5, pady=5, sticky="e")
 
-        tk.Label(self.form_frame, text="Nouveau mot de passe").pack()
-        self.new_password = tk.Entry(self.form_frame, show="*")
-        self.new_password.pack()
+        tk.Label(
+            self.form_frame,
+            text="Nouveau mot de passe",
+            bg="#121212",
+            fg="white",
+            font=("Arial", 11)
+        ).grid(row=2, column=0, padx=5, pady=5, sticky="e")
+        self.new_password = tk.Entry(
+            self.form_frame,
+            width=30,
+            bg="#2B2B2B",
+            fg="white",
+            insertbackground="white",
+            show="*"
+        )
+        self.new_password.grid(row=2, column=1, padx=5, pady=5, sticky="e")
 
         tk.Button(
             self.form_frame,
-            text="Valider",
+            text="[ Valider ]",
+            width=25,
+            height=2,
+            bg="#00A86B",
+            fg="white",
+            font=("Arial", 11, "bold"),
             command=self.update_passwd
-        ).pack()
+        ).grid(row=3, column=0, columnspan=2, pady=15)
 
     def update_passwd(self):
         username = self.username.get().strip()
         passwd = self.password.get().strip()
         new_passwd = self.new_password.get().strip()
-
-        if not re.match("^[a-zA-Z0-9]+$", username) :
-            self.show_message("Le nom d'utilisateur contient des caractères non autorisés")
-            return
 
         try:
 
@@ -243,6 +452,7 @@ class SettingsPage(tk.Frame):
             self.show_message(e)
             print(str(e))
             return
+
 
     def add_subject_form(self):
         self.clear_form()
@@ -417,6 +627,8 @@ class SettingsPage(tk.Frame):
             self.show_message(e)
             print(str(e))
             return
+
+
 
     def add_question_form(self):
         self.clear_form()
@@ -702,6 +914,7 @@ class SettingsPage(tk.Frame):
             self.show_message(e)
             print(str(e))
 
+
     def delete_question_form(self):
         self.clear_form()
         self.show_message("")
@@ -837,6 +1050,8 @@ class SettingsPage(tk.Frame):
 
     def add_answer_form(self):
         pass
+
+
 
     def aff_quest_answer(self):
         pass
